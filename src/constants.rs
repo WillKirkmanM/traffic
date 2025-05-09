d0778bdf91258d348dbc2a884b9ee4687a5f7dc9
WillKirkmanM <WillKirkmanMoller@gmail.com>
2025-05-09 23:22:18 +0100

    Feat(constants.rs): Declare Traffic Simulation Constants

---
+++ src/constants.rs
@@ -0,0 +1,15 @@
+pub const WIDTH: usize = 800;
+pub const HEIGHT: usize = 600;
+pub const ROAD_WIDTH: usize = 50;
+pub const VEHICLE_LENGTH: usize = 20;
+pub const VEHICLE_WIDTH: usize = 10;
+pub const TRAFFIC_LIGHT_SIZE: usize = 15;
+pub const SIMULATION_SPEED: f32 = 1.0;
+
+pub const COLOR_BACKGROUND: u32 = 0x00333333;
+pub const COLOR_ROAD: u32 = 0x00666666;
+pub const COLOR_ROAD_MARKING: u32 = 0x00FFFFFF;
+pub const COLOR_VEHICLE: u32 = 0x00FFAA00;
+pub const COLOR_LIGHT_RED: u32 = 0x00FF0000;
+pub const COLOR_LIGHT_YELLOW: u32 = 0x00FFFF00;
+pub const COLOR_LIGHT_GREEN: u32 = 0x0000FF00;
